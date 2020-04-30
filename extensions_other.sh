eecea99404f60d528d039e8b2e55bb81caee9af7
Tim Paine <t.paine154@gmail.com>
2020-04-30 11:56:17 -0400

    updates for jlab2

---
+++ extensions_other.sh
@@ -0,0 +1,30 @@
+# NON JLAB2
+# @alexbella/jupyterlab-custom-theme
+# @jupyterlab/google-drive
+# @mflevine/jupyterlab_html
+# @oriolmirosa/jupyterlabjupyterlab_materialdarker
+# @telamonian/theme-darcula
+# jupyterlab-cpustatus
+# jupyterlab-dash
+# jupyterlab-hdf
+# jupyterlab-heroku
+# jupyterlab-prodigy
+# jupyterlab_autoversion
+# jupyterlab_celltests
+
+# BROKEN
+# @quentinandre/jupyterlab_snippets
+# jupyterlab_tensorboard
+# @rahlir/theme-gruvbox
+# @kenshohara/theme-nord-extension
+# jupyterlab_discovery
+# @deatbeds/jupyterlab_graphviz
+# @shahinrostami/theme-purple-please
+# jupyterlab_voyager@1.0.2
+# jupyterlab-nvdashboard@0.1.9
+
+# DEPRECATED
+# beakerx_table-jupyterlab
+# pylantern
+# jupyter-combobox@0.1.1
+
